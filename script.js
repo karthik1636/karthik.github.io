f574d9f581fa3b64a486aa6e56e8d52040c56a81
karthik1636 <149125097+karthik1636@users.noreply.github.com>
2024-03-29 15:59:19 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent the default form submission
+    alert('Form submitted! We will be in touch.');
+});
